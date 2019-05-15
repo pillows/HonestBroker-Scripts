626a14e5659979814bcb0772bf870ea776ccec0b
matthe05 <matthe05@domainone-1.broker-project.ch-geni-net.instageni.arc.vt.edu>
2019-05-14 22:00:44 -0700

    Finished paths for cpu stats and stress test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var os = require('os-utils');
-
+const shell = require('shelljs');
 
 app.get("/cpu", (req, res, next) => {
   os.cpuUsage(function(v){
@@ -10,6 +10,14 @@ app.get("/cpu", (req, res, next) => {
 
 });
 
+app.get("/cpuTest", (req, res, next) => {
+	let time = req.query.time;
+	let cputest = req.query.percentage;
+	console.log(time, cputest)
+	console.log("stress -c 1 -t " + time + " & cpulimit -p $( pidof -o $! stress ) -l " + cputest)
+	shell.exec("stress -c 1 -t " + time + " & cpulimit -p $( pidof -o $! stress ) -l " + cputest)
+	return res.send("1")
+});
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
